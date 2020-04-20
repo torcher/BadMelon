e703997409bbcf0c367abeee9f7b793cc512dfb4
Torch <joshua.geurts@gmail.com>
2020-04-20 18:22:44 -0400

    Initial commit

---
+++ BadMelon.API/Controllers/RecipeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BadMelon.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BadMelon.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class RecipeController : Controller
+    {
+        private static readonly Recipe[] Recipes = new Recipe[]
+            {
+                new Recipe{ ID = System.Guid.NewGuid(), Name = "Potato Soup", Ingredients = $"Potatos{Environment.NewLine}Soup", Steps = $"Mix and serve"},
+                new Recipe{ ID = System.Guid.NewGuid(), Name = "Bacon", Ingredients = $"Bacon", Steps = $"Cook bacon."},
+                new Recipe{ ID = System.Guid.NewGuid(), Name = "Sandwich", Ingredients = $"Bread{Environment.NewLine}Innards", Steps = $"Mix ad serve"}
+            };
+
+        [HttpGet]
+        public IEnumerable<Recipe> Get()
+        {
+            return Recipes;
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public Recipe Get(Guid id)
+        {
+            return Recipes.SingleOrDefault(recipe => recipe.ID == id);
+        }
+    }
+}
---
+++ BadMelon.API/Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BadMelon.API.Models
+{
+    public class Recipe
+    {
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Ingredients { get; set; }
+
+        public string Steps { get; set; }
+    }
+}
