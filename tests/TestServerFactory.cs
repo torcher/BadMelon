621429ed84844c5f6d75fce6199c9f9ce361869f
Torch <joshua.geurts@gmail.com>
2020-06-10 19:08:53 -0400

    Fixed issue with running tests. Was looking for an ilogger interface to be injected but wasn't registered in the TestStartup configuration.

--- tests/TestServerFactory.cs
+++ tests/TestServerFactory.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Serilog;
 
 namespace BadMelon.Tests
 {
     internal class TestServerFactory
     {
-        public static TestServer TestServer = new TestServer(WebHost.CreateDefaultBuilder().UseStartup<TestStartup>().UseEnvironment("Testing"));
+        public static TestServer TestServer = new TestServer(WebHost.CreateDefaultBuilder().UseStartup<TestStartup>().UseSerilog().UseEnvironment("Testing"));
     }
 }
