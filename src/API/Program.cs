b87f5ad71d891e2b1a0eff5678d201285eb530bf
Torch <joshua.geurts@gmail.com>
2020-06-10 19:00:57 -0400

    Added serilog and support for request and error logging.

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+using System;
 
 namespace BadMelon.API
 {
@@ -7,11 +11,30 @@ namespace BadMelon.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+               .WriteTo.File(new RenderedCompactJsonFormatter(), "logs/seri.log", rollingInterval: RollingInterval.Day)
+               .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
