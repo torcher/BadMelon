59c55137ead15167a3ff385cef26dc789bcf845a
Torch <joshua.geurts@gmail.com>
2021-07-25 11:34:47 -0400

    Changed sample data to use static GUIDs for reference in test data. Added U and D endpoints, service/repository functionality and relevant tests.

--- src/Data/DataSamples.cs
+++ src/Data/DataSamples.cs
@@ -7,8 +7,8 @@ namespace BadMelon.Data
 {
     public class DataSamples
     {
-        public (User, DTOs.Login)[] Users { get => _Users?.ToArray() ?? new (User, DTOs.Login)[] { }; }
-        private List<(User, DTOs.Login)> _Users;
+        public (User user, DTOs.Login login)[] Users { get => _Users?.ToArray() ?? new (User user, DTOs.Login login)[] { }; }
+        private List<(User user, DTOs.Login login)> _Users;
 
         public Recipe[] Recipes { get => _Recipes.ToArray(); }
         private List<Recipe> _Recipes;
@@ -30,43 +30,43 @@ namespace BadMelon.Data
             var joshUser = new User { UserName = "jge", Email = "joshua.geurts@gmail.com", EmailConfirmed = true, IsPasswordSet = true };
             var joshUserLogin = new DTOs.Login { Username = "jge", Password = "Qwerty123!Qwerty123!Qwerty123!" };
 
-            _Users = new List<(User, DTOs.Login)>();
+            _Users = new List<(User user, DTOs.Login login)>();
             _Users.Add((user0, user0Login));
             _Users.Add((user1, user1Login));
             _Users.Add((user2_passwordNotConfirmed, user2Login));
             _Users.Add((joshUser, joshUserLogin));
 
             _IngredientTypes = new List<IngredientType>();
-            var water = new IngredientType { ID = Guid.NewGuid(), Name = "Water" };
-            var corn = new IngredientType { ID = Guid.NewGuid(), Name = "Corn" };
+            var water = new IngredientType { ID = Guid.Parse("36de307c-1003-4200-a243-604d31ccd9de"), Name = "Water" };
+            var corn = new IngredientType { ID = Guid.Parse("b233e6fe-82ab-4e2c-b13d-0639365a5e9b"), Name = "Corn" };
             _IngredientTypes.AddRange(new IngredientType[] { water, corn });
 
             var cornSoup = new Recipe
             {
-                ID = Guid.NewGuid(),
+                ID = Guid.Parse("bcede8a8-22e8-41a7-995a-f599052a35eb"),
                 User = user0,
                 Name = "Cold Corn Soup",
                 Ingredients = new List<Ingredient>(),
                 Steps = new List<Step>()
             };
 
-            cornSoup.Ingredients.Add(new Ingredient { ID = Guid.NewGuid(), IngredientType = corn, IngredientTypeID = corn.ID, Weight = 1d });
-            cornSoup.Ingredients.Add(new Ingredient { ID = Guid.NewGuid(), IngredientType = water, IngredientTypeID = water.ID, Weight = 2d });
-            cornSoup.Steps.Add(new Step { ID = Guid.NewGuid(), Order = 1, Recipe = cornSoup, RecipeId = cornSoup.ID, Text = "Mix Corn and Water" });
-            cornSoup.Steps.Add(new Step { ID = Guid.NewGuid(), Order = 2, Recipe = cornSoup, RecipeId = cornSoup.ID, Text = "Serve" });
+            cornSoup.Ingredients.Add(new Ingredient { ID = Guid.Parse("c54d4bf8-5e85-48de-959d-4cfe6ef5f206"), IngredientType = corn, IngredientTypeID = corn.ID, Weight = 1d });
+            cornSoup.Ingredients.Add(new Ingredient { ID = Guid.Parse("41dd3cdb-8552-4bf8-bb12-4dfce5492fb4"), IngredientType = water, IngredientTypeID = water.ID, Weight = 2d });
+            cornSoup.Steps.Add(new Step { ID = Guid.Parse("a6b2cde9-95c4-4840-91e3-1db7d005d9e0"), Order = 1, Recipe = cornSoup, RecipeId = cornSoup.ID, Text = "Mix Corn and Water" });
+            cornSoup.Steps.Add(new Step { ID = Guid.Parse("8c3156aa-ff28-48d9-95a4-ec6de51693a1"), Order = 2, Recipe = cornSoup, RecipeId = cornSoup.ID, Text = "Serve" });
 
             var hotWater = new Recipe
             {
-                ID = Guid.NewGuid(),
+                ID = Guid.Parse("1e8a7973-d161-436a-90d4-b9e2b79fcf2c"),
                 User = user0,
                 Name = "Hot Water",
                 Ingredients = new List<Ingredient>(),
                 Steps = new List<Step>()
             };
-            hotWater.Ingredients.Add(new Ingredient { ID = Guid.NewGuid(), IngredientType = water, IngredientTypeID = water.ID, Weight = 1d });
+            hotWater.Ingredients.Add(new Ingredient { ID = Guid.Parse("bac2506f-5acf-4703-b88d-cee25d1e64aa"), IngredientType = water, IngredientTypeID = water.ID, Weight = 1d });
             hotWater.Steps.Add(new Step
             {
-                ID = Guid.NewGuid(),
+                ID = Guid.Parse("bdf0c3f7-665d-4967-8fb8-027e64c3572c"),
                 Order = 1,
                 Text = "Heat water",
                 CookTime = new TimeSpan(0, 1, 0),
@@ -77,16 +77,16 @@ namespace BadMelon.Data
 
             var coldWater = new Recipe
             {
-                ID = Guid.NewGuid(),
+                ID = Guid.Parse("89125059-59cd-4fbf-91a1-f9691a8e86a9"),
                 User = user0,
                 Name = "Cold Water",
                 Ingredients = new List<Ingredient>(),
                 Steps = new List<Step>()
             };
-            coldWater.Ingredients.Add(new Ingredient { ID = Guid.NewGuid(), IngredientType = water, IngredientTypeID = water.ID, Weight = 1d });
+            coldWater.Ingredients.Add(new Ingredient { ID = Guid.Parse("1d809fa4-f146-4f8c-a3e4-d278ec8e0ef7"), IngredientType = water, IngredientTypeID = water.ID, Weight = 1d });
             coldWater.Steps.Add(new Step
             {
-                ID = Guid.NewGuid(),
+                ID = Guid.Parse("a79ffd57-86f3-44ab-9088-dcabcffe03a5"),
                 Order = 1,
                 Text = "Cool water",
                 CookTime = new TimeSpan(0, 1, 0),
---
+++ tests/Data/UpdateRecipeTestData.cs
@@ -0,0 +1,138 @@
+﻿using BadMelon.Data.DTOs;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BadMelon.Tests.Data
+{
+    public class UpdateRecipeTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]{ new Recipe
+            {
+                ID = Guid.Parse("bcede8a8-22e8-41a7-995a-f599052a35eb"),
+                Name = "Cold Corn Soupa",
+                Ingredients = new List<Ingredient> {
+                    new Ingredient { TypeID = Guid.Parse("36de307c-1003-4200-a243-604d31ccd9de"), Type = "Water", Weight = 2d },
+                    new Ingredient { TypeID = Guid.Parse("b233e6fe-82ab-4e2c-b13d-0639365a5e9b"), Type = "Corn", Weight = 1d }
+                },
+                Steps = new List<Step> {
+                    new Step { Order = 1, Text = "Mix Corn and Water" },
+                    new Step { Order = 2, Text = "Serve" }
+                }
+            } };
+
+            yield return new object[]{ new Recipe
+            {
+                ID = Guid.Parse("bcede8a8-22e8-41a7-995a-f599052a35eb"),
+                Name = "Cold Corn Soupa",
+                Ingredients = new List<Ingredient> {
+                    new Ingredient { TypeID = Guid.Parse("36de307c-1003-4200-a243-604d31ccd9de"), Type = "Water", Weight = 3d },
+                    new Ingredient { TypeID = Guid.Parse("b233e6fe-82ab-4e2c-b13d-0639365a5e9b"), Type = "Corn", Weight = 1d }
+                },
+                Steps = new List<Step> {
+                    new Step { Order = 1, Text = "Mix Corn and Water" },
+                    new Step { Order = 2, Text = "Serve" }
+                }
+            } };
+
+            yield return new object[]{ new Recipe
+            {
+                ID = Guid.Parse("bcede8a8-22e8-41a7-995a-f599052a35eb"),
+                Name = "Cold Corn Soupa",
+                Ingredients = new List<Ingredient> {
+                    new Ingredient { TypeID = Guid.Parse("b233e6fe-82ab-4e2c-b13d-0639365a5e9b"), Type = "Corn", Weight = 1d }
+                },
+                Steps = new List<Step> {
+                    new Step { Order = 1, Text = "Mix Corn and Water" },
+                    new Step { Order = 2, Text = "Serve" }
+                }
+            } };
+
+            yield return new object[]{ new Recipe
+            {
+                ID = Guid.Parse("bcede8a8-22e8-41a7-995a-f599052a35eb"),
+                Name = "Cold Corn Soupa",
+                Ingredients = new List<Ingredient> {
+                    new Ingredient { TypeID = Guid.Parse("36de307c-1003-4200-a243-604d31ccd9de"), Type = "Water", Weight = 1d }
+                },
+                Steps = new List<Step> {
+                    new Step { Order = 1, Text = "Mix Corn and Water" },
+                    new Step { Order = 2, Text = "Serve" }
+                }
+            } };
+
+            yield return new object[]{ new Recipe
+            {
+                ID = Guid.Parse("bcede8a8-22e8-41a7-995a-f599052a35eb"),
+                Name = "Cold Corn Soupa",
+                Ingredients = new List<Ingredient> {
+                    new Ingredient { TypeID = Guid.Parse("36de307c-1003-4200-a243-604d31ccd9de"), Type = "Water", Weight = 2d } ,
+                    new Ingredient { TypeID = Guid.Parse("b233e6fe-82ab-4e2c-b13d-0639365a5e9b"), Type = "Corn", Weight = 1d }
+                },
+                Steps = new List<Step> {
+                    new Step { Order = 1, Text = "Mix Corn and Water" },
+                    new Step { Order = 2, Text = "Serve" }
+                }
+            } };
+
+            yield return new object[]{ new Recipe
+            {
+                ID = Guid.Parse("bcede8a8-22e8-41a7-995a-f599052a35eb"),
+                Name = "Cold Corn Soupa",
+                Ingredients = new List<Ingredient> {
+                    new Ingredient { TypeID = Guid.Parse("36de307c-1003-4200-a243-604d31ccd9de"), Type = "Water", Weight = 2d },
+                    new Ingredient { TypeID = Guid.Parse("b233e6fe-82ab-4e2c-b13d-0639365a5e9b"), Type = "Corn", Weight = 1d }
+                },
+                Steps = new List<Step> {
+                    new Step { Order = 1, Text = "Mix Corn and Water" },
+                    new Step { Order = 2, Text = "Serve cold" }
+                }
+            } };
+
+            yield return new object[]{ new Recipe
+            {
+                ID = Guid.Parse("bcede8a8-22e8-41a7-995a-f599052a35eb"),
+                Name = "Cold Corn Soupa",
+                Ingredients = new List<Ingredient> {
+                    new Ingredient { TypeID = Guid.Parse("36de307c-1003-4200-a243-604d31ccd9de"), Type = "Water", Weight = 2d },
+                    new Ingredient { TypeID = Guid.Parse("b233e6fe-82ab-4e2c-b13d-0639365a5e9b"), Type = "Corn", Weight = 1d }
+                },
+                Steps = new List<Step> {
+                    new Step { Order = 1, Text = "Serve cold" },
+                    new Step { Order = 2, Text = "Mix Corn and Water" }
+                }
+            } };
+
+            yield return new object[]{ new Recipe
+            {
+                ID = Guid.Parse("bcede8a8-22e8-41a7-995a-f599052a35eb"),
+                Name = "Cold Corn Soupa",
+                Ingredients = new List<Ingredient> {
+                    new Ingredient { TypeID = Guid.Parse("36de307c-1003-4200-a243-604d31ccd9de"), Type = "Water", Weight = 2d },
+                    new Ingredient { TypeID = Guid.Parse("b233e6fe-82ab-4e2c-b13d-0639365a5e9b"), Type = "Corn", Weight = 1d }
+                },
+                Steps = new List<Step> {
+                    new Step { Order = 1, Text = "Mix Corn and Water" }
+                }
+            } };
+
+            yield return new object[]{ new Recipe
+            {
+                ID = Guid.Parse("bcede8a8-22e8-41a7-995a-f599052a35eb"),
+                Name = "Cold Corn Soupa",
+                Ingredients = new List<Ingredient> {
+                    new Ingredient { TypeID = Guid.Parse("36de307c-1003-4200-a243-604d31ccd9de"), Type = "Water", Weight = 2d },
+                    new Ingredient { TypeID = Guid.Parse("b233e6fe-82ab-4e2c-b13d-0639365a5e9b"), Type = "Corn", Weight = 1d }
+                },
+                Steps = new List<Step> {
+                    new Step { Order = 1, Text = "Serve" },
+                    new Step { Order = 2, Text = "Mix Corn and Water" }
+                }
+            } };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
